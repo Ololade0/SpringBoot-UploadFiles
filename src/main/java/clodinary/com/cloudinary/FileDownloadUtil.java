f5dbde270aeae96e5d9c18d391f36e7ea8edc189
Ololade0 <adesuyiololade@gmail.com>
2023-01-12 09:16:34 -0800

    Uploading files

--- src/main/java/clodinary/com/cloudinary/FileDownloadUtil.java
+++ src/main/java/clodinary/com/cloudinary/FileDownloadUtil.java
@@ -1,2 +1,26 @@
-package clodinary.com.cloudinary;public class FileDownloadUtil {
+package clodinary.com.cloudinary;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileDownloadUtil {
+   private Path foundFile;
+    public Resource getFileAsResource(String fileCode) throws IOException {
+        Path uploadDirector = Paths.get("Files-Upload");
+        Files.list(uploadDirector).forEach(file -> {
+          if(file.getFileName().toString().startsWith(fileCode)){
+              return;
+          }
+        });
+        if(foundFile != null){
+            return new UrlResource(foundFile.toUri());
+        }
+        return null;
+    }
 }
--- src/main/java/clodinary/com/cloudinary/FileUploadResponse.java
+++ src/main/java/clodinary/com/cloudinary/FileUploadResponse.java
@@ -1,2 +1,31 @@
-package clodinary.com.cloudinary;public class FileUploadResponse {
+package clodinary.com.cloudinary;
+
+public class FileUploadResponse {
+    private String fileName;
+    private String downloadUri;
+    private long size;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getDownloadUri() {
+        return downloadUri;
+    }
+
+    public void setDownloadUri(String downloadUri) {
+        this.downloadUri = downloadUri;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
 }
